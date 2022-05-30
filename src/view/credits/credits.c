ded631e1016518a5eeca1945c714a4422868d18b
Benjamin Hautier <benjamin.hautier@etudiant.univ-reims.fr>
2022-05-30 10:17:45 +0200

    end of credits page

--- src/view/credits/credits.c
+++ src/view/credits/credits.c
@@ -3,21 +3,27 @@
 #include "./../utils/utils.h"
 #include <stdio.h>
 
-static float creditsScrollingPosY = 0;
-
 #define TAB_SIZE 4*2
+
+static float creditsScrollingPosY = 0;
+static Texture2D raylibLogo;
 
 void InitCreditsScreen(void)
 {
     printf("Credits Screen Init\n");
 
     creditsScrollingPosY = (float)GetScreenHeight() + 20.0f;
+    raylibLogo = LoadTexture("./asset/Misc/raylib_logo.png");
     
 }
 void UpdateCreditsScreen(void)
 {
-    creditsScrollingPosY -= 0.6f;
-    if (creditsScrollingPosY < (-GetScreenHeight() + 36)) creditsScrollingPosY = -GetScreenHeight() + 36;
+    creditsScrollingPosY -= 0.8f;
+    if (IsKeyPressed(KEY_SPACE) || IsKeyDown(KEY_DOWN))
+    {
+        creditsScrollingPosY -= 3.0f;
+    }
+    if (creditsScrollingPosY < (-GetScreenHeight() -400)) creditsScrollingPosY = -GetScreenHeight() - 400;
     
 }
 void DrawCreditsScreen(void)
@@ -28,36 +34,43 @@ void DrawCreditsScreen(void)
     int fontSize = 50;
 
     char *creditsData[TAB_SIZE] = { "DESIGN DU JEU",
-                        "Hugo Dutoit\nHautier Benjamin",
+                        "Dutoit Hugo\nHautier Benjamin",
                         "DEVELOPPEMENT DU JEU",
-                        "Léonard Corre\nMartin Mille\nHugo Dutoit\nHautier Benjamin",
+                        "Corre Léonard\nMille Martin\nDutoit Hugo\nHautier Benjamin",
                         "CREATION D'ASSETS",
-                        "hugo Dutoit\nHautier Benjamin",
-                        "MUSIC",
+                        "Dutoit Hugo\nHautier Benjamin",
+                        "MUSIQUE PAR",
                         "Dar Golan" };
 
     float padding = 20.0f; 
     float sizeQueue = 0.0f;
+    Color color = GetColor(0x8a9696ff);
     for (int i = 0; i < TAB_SIZE-1; i+=2) {
         int factorPos = i != 0 ? i : 1;
         Vector2 sectionTitleSize = MeasureTextEx(font, creditsData[i], fontSize, 2);
         Vector2 sectionDataSize = MeasureTextEx(font, creditsData[i+1], fontSize, 2);
         sizeQueue += 20.0f;
-        DrawTextEx(font, creditsData[i], (Vector2){ GetScreenWidth()/2 - sectionTitleSize.x/2, creditsScrollingPosY + sizeQueue }, fontSize, 2, GetColor(0x8a9696ff));
+        DrawTextEx(font, creditsData[i], (Vector2){ GetScreenWidth()/2 - sectionTitleSize.x/2, creditsScrollingPosY + sizeQueue }, fontSize, 2, color);
         sizeQueue += sectionTitleSize.y;
         DrawTextEx(font, creditsData[i+1], (Vector2){ GetScreenWidth()/2 - sectionDataSize.x/2, creditsScrollingPosY + sizeQueue }, fontSize, 2, WHITE);
         sizeQueue += sectionDataSize.y;
     }
 
-    //DrawTextEx(font, "powered by", (Vector2){ GetScreenWidth()/2 - MeasureTextEx(font, "powered by", fontSize, 2).x/2, creditsScrollingPosY + 240 }, fontSize, 2, GRAY);
-    //DrawTexture(texLogo, GetScreenWidth()/2 - 64, creditsScrollingPosY + 256, WHITE);
+    Vector2 sectionTitleSize = MeasureTextEx(font, "Crée avec", fontSize, 2);
+    DrawTextEx(font, "Crée avec", (Vector2){ GetScreenWidth()/2 - sectionTitleSize.x/2, creditsScrollingPosY + sizeQueue + 20 }, fontSize, 2, color);
+    sizeQueue += sectionTitleSize.y;
+    DrawTexture(raylibLogo, GetScreenWidth()/2 - 64, creditsScrollingPosY + sizeQueue + 40, WHITE);
+
+    int buttonWitdth = 200;
+    int buttonHeight = 80;
+    if (GuiButton((Rectangle){ GetScreenWidth() - (buttonWitdth+20), GetScreenHeight()-(buttonHeight+20), buttonWitdth, buttonHeight }, "RETOUR", -1)) TransitionToScreen(MENU);
 
 }
 void UnloadCreditsScreen(void)
 {
     
 }
-int FinishCreditsScreen(void)
+int FinishCreditsScreen(void) 
 {
     return 0;
 }

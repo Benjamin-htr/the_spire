1b4a7b7a2be8b750f27b293508acf857d673b47a
Benjamin Hautier <benjamin.hautier@etudiant.univ-reims.fr>
2022-06-08 11:41:44 +0200

    ajout sprite map

--- src/view/gameplay/gameplay.c
+++ src/view/gameplay/gameplay.c
@@ -9,9 +9,17 @@
 //----------------------------------------------------------------------------------
 static int finishScreen = 0;
 
+static Texture2D arrowButton = { 0 };
+
+static Sprite roomSprite = { 0 };
+
 void InitGameplayScreen(void)
 {
     printf("Gameplay Screen Init\n");
+
+    arrowButton = LoadTexture("./asset/UI_assets/arrow.png");
+
+    constructSprite(&roomSprite, "./asset/map/room.png", 3, 1, (Vector2){GetScreenWidth()/2, GetScreenHeight()/2});
     
 }
 void UpdateGameplayScreen(void)
@@ -20,14 +28,29 @@ void UpdateGameplayScreen(void)
     {
         showInGameMenu = !showInGameMenu;
     }
+    
+    updateSprite(&roomSprite);
+
+
 }
 void DrawGameplayScreen(void)
 {
     DrawText("GAMEPLAY SCREEN", 190, 200, 20, LIGHTGRAY);
+
+    float scaleFactor = 4.0f;
+    DrawTextureEx(arrowButton, (Vector2){ GetScreenWidth()/2 - arrowButton.width*scaleFactor/2, GetScreenHeight()/2 - arrowButton.height*scaleFactor/2 }, 0, scaleFactor, WHITE);
+
+
+    const float scaleFactor = 1.0f;
+
+    const Vector2 position = { GetScreenWidth()/2 - (roomSprite.frameRec.width*scaleFactor)/2, GetScreenHeight()/2 - (roomSprite.frameRec.height*scaleFactor)/2 };
+    drawSprite(&roomSprite, position, 0.0f, scaleFactor, WHITE);
+
+
 }
 void UnloadGameplayScreen(void)
 {
-    
+    UnloadTexture(arrowButton);
 }
 int FinishGameplayScreen(void)
 {

8d0dc80800f93f6b4d6f211e16e80b14bdf54a2b
hugo dutoit <dutoit.hugo2001@gmail.com>
2022-06-08 06:22:05 +0000

    cards global variables

--- src/model/game/misc/deck/card/card.h
+++ src/model/game/misc/deck/card/card.h
@@ -1,17 +1,27 @@
 #ifndef ITEM
 #define ITEM
 
-#include "../effect/effect.h"
+#include "../../effect/effect.h"
+
+enum rarityE{
+    COMMON,
+    BASIC,
+    ATYPIC,
+    RARE,
+};
 
 typedef struct
 {
     char* name;
-    int manaCoast;
+    enum rarityE rarity;
+    int manaCost;
     int energyCost;
     effect* effects;
     char* technic;
     char* description;
 } card;
+
+card * createCard(char* name,enum rarityE rarity, int manaCost, int energyCost , effect* effects, char* technic,char* description);
 
 
 #endif

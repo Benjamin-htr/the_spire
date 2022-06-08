465d738444648712ddd5cb375ed5625a046ecedc
Benjamin Hautier <benjamin.hautier@etudiant.univ-reims.fr>
2022-06-09 00:38:35 +0200

    debut affichage rooms

--- src/view/gameplay/gameplay.c
+++ src/view/gameplay/gameplay.c
@@ -19,7 +19,7 @@ void InitGameplayScreen(void)
 
     arrowButton = LoadTexture("./asset/UI_assets/arrow.png");
 
-    constructSprite(&roomSprite, "./asset/map/room.png", 3, 1, (Vector2){GetScreenWidth() / 2, GetScreenHeight() / 2});
+    constructSprite(&roomSprite, "./asset/map/room_start.png", 3, 1, (Vector2){GetScreenWidth() / 2, GetScreenHeight() / 2});
 }
 void UpdateGameplayScreen(void)
 {
@@ -32,12 +32,15 @@ void UpdateGameplayScreen(void)
 }
 void DrawGameplayScreen(void)
 {
-    DrawText("GAMEPLAY SCREEN", 190, 200, 20, LIGHTGRAY);
+    ClearBackground(BLACK);
+
+    Vector2 textSize = MeasureTextEx(font, "ETAGE 1", 20, 1);
+    DrawTextEx(font, "ETAGE 1", (Vector2){GetScreenWidth()- (textSize.x+10), GetScreenHeight()-(textSize.y+10)}, 20, 1, LIGHTGRAY);
 
     float scaleFactor = 4.0f;
     DrawTextureEx(arrowButton, (Vector2){GetScreenWidth() / 2 - arrowButton.width * scaleFactor / 2, GetScreenHeight() / 2 - arrowButton.height * scaleFactor / 2}, 0, scaleFactor, WHITE);
 
-    scaleFactor = 1.0f;
+    scaleFactor = 1.2f;
 
     const Vector2 position = {GetScreenWidth() / 2 - (roomSprite.frameRec.width * scaleFactor) / 2, GetScreenHeight() / 2 - (roomSprite.frameRec.height * scaleFactor) / 2};
     drawSprite(&roomSprite, position, 0.0f, scaleFactor, WHITE);
